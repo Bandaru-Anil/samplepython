c5b3dbafbfcc1967117e93c5cdb5164451bfcfa4
Anil Bandaru <bandaruanil1997@gmail.com>
2023-09-13 14:42:54 +0530

    iterated the list of numbers using loop

--- Downloads/github/sample.py
+++ Downloads/github/sample.py
@@ -2,4 +2,7 @@ a = 10
 b = 20 
 c = a+b
 print("the sum of two variables is :",c)
-print("this is my first project")
+print("this is my first projec")
+lst = range(1,20)
+for i in lst:
+    print(i) 
